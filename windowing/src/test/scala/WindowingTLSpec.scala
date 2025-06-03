da5bccd8dc8ae53bc7cf9665a97eb3868c396e3f
opera-platform <p.z.nikola@etf.bg.ac.rs>
2025-06-03 16:49:51 +0200

    Added TL tests

---
+++ windowing/src/test/scala/WindowingTLSpec.scala
@@ -0,0 +1,100 @@
+package windowing
+
+import chiseltest.{ChiselScalatestTester, TreadleBackendAnnotation, VerilatorBackendAnnotation, WriteVcdAnnotation}
+import dsptools.numbers.Convergent
+import fixedpoint._
+import freechips.rocketchip.diplomacy._
+import freechips.rocketchip.tilelink.TLBundleParameters
+import org.chipsalliance.cde.config.Parameters
+import org.chipsalliance.diplomacy.lazymodule.LazyModule
+import org.scalatest.flatspec.AnyFlatSpec
+
+class WindowingTLSpec extends AnyFlatSpec with ChiselScalatestTester {
+  behavior of "Windowing-TL"
+
+  implicit val p: Parameters = Parameters.empty
+  val annotations = Seq(WriteVcdAnnotation, VerilatorBackendAnnotation)
+
+  val beatBytes = 4
+  val verbose   = false
+  val random    = true
+
+  for (enable <- Seq(false, true)) {
+    for (numPoints <- Seq(32, 128, 256, 1024, 2048)) {
+      for (window <- Seq(
+        TriangularWindow(numPoints, periodic = true),
+        HammingWindow(numPoints, periodic = true),
+        HanningWindow(numPoints, periodic = true),
+        BlackmanWindow(numPoints, periodic = true),
+        GaussianWindow(numPoints, sigma = 0.5, periodic = true),
+        CustomWindow(f"src/main/resources/custom_$numPoints.txt"),
+        NoWindow()
+      )) {
+        for (constWindow <- Seq(false, true)) {
+          for (runTime <- Seq(false, true)) {
+            // Address
+            val ramAddress = AddressSet(0x60000, numPoints * 4 - 1)
+            val csrAddress = AddressSet(0x60000 + numPoints * 4, 0xF)
+            // Parameters
+            val params = WindowingParams.fixed(
+              numPoints = numPoints,
+              dataWidth = 16,
+              binPoint = 14,
+              runTime = runTime,
+              constWindow = constWindow,
+              trimType = Convergent,
+              memoryFile = s"./test_run_dir/window_$numPoints.txt",
+              windowFunc = window
+            )
+
+            it should "pass when: \n" +
+              s"\t\twindow enabled = $enable,\n" +
+              s"\t\twindow type    = ${window.toString},\n" +
+              s"\t\twindow size    = $numPoints,\n" +
+              s"\t\tconst window   = $constWindow,\n" +
+              s"\t\trun-time       = $runTime,\n" in {
+              val lazyDut = LazyModule(
+                new WindowingTL[FixedPoint](
+                  csrAddress = csrAddress,
+                  ramAddress = ramAddress,
+                  params = params,
+                  beatBytes = beatBytes
+                ) with TestStandaloneTLBlock {
+                  override def standaloneParams: TLBundleParameters =
+                    TLBundleParameters(
+                      addressBits = beatBytes*8,
+                      dataBits = beatBytes*8,
+                      sourceBits = 4,
+                      sinkBits = 1,
+                      sizeBits = 2,
+                      echoFields = Nil,
+                      requestFields = Nil,
+                      responseFields = Nil,
+                      hasBCE = false
+                    )
+                  override def dataBytes: Int = 4
+                }
+              )
+
+              test(lazyDut.module)
+                .withAnnotations(annotations)
+                .runPeekPoke(_ =>
+                  new WindowingTLTester(
+                    lazyDut,
+                    csrAddress = csrAddress,
+                    ramAddress = ramAddress,
+                    params = params,
+                    windowFuncRunTime = BlackmanWindow(numPoints / 2, periodic = true),
+                    beatBytes = beatBytes,
+                    enable = enable,
+                    verbose = verbose,
+                    random = random
+                  )
+                )
+            }
+          }
+        }
+      }
+    }
+  }
+}
