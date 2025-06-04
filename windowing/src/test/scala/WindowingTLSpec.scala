986ac098c9b9dc826593fab5bc3c108c6c9d65ea
opera-platform <p.z.nikola@etf.bg.ac.rs>
2025-06-04 20:05:12 +0200

    Update TL tests to test different binary point options

--- windowing/src/test/scala/WindowingTLSpec.scala
+++ windowing/src/test/scala/WindowingTLSpec.scala
@@ -1,7 +1,7 @@
 package windowing
 
 import chiseltest.{ChiselScalatestTester, TreadleBackendAnnotation, VerilatorBackendAnnotation, WriteVcdAnnotation}
-import dsptools.numbers.Convergent
+import dsptools.numbers.{Ceiling, Convergent, Floor, Round}
 import fixedpoint._
 import freechips.rocketchip.diplomacy._
 import freechips.rocketchip.tilelink.TLBundleParameters
@@ -20,7 +20,7 @@ class WindowingTLSpec extends AnyFlatSpec with ChiselScalatestTester {
   val random    = true
 
   for (enable <- Seq(false, true)) {
-    for (numPoints <- Seq(32, 128, 256, 1024, 2048)) {
+    for (numPoints <- Seq(2048)) {
       for (window <- Seq(
         TriangularWindow(numPoints, periodic = true),
         HammingWindow(numPoints, periodic = true),
@@ -32,65 +32,72 @@ class WindowingTLSpec extends AnyFlatSpec with ChiselScalatestTester {
       )) {
         for (constWindow <- Seq(false, true)) {
           for (runTime <- Seq(false, true)) {
-            // Address
-            val ramAddress = AddressSet(0x60000, numPoints * 4 - 1)
-            val csrAddress = AddressSet(0x60000 + numPoints * 4, 0xF)
-            // Parameters
-            val params = WindowingParams.fixed(
-              numPoints = numPoints,
-              dataWidth = 16,
-              binPoint = 14,
-              runTime = runTime,
-              constWindow = constWindow,
-              trimType = Convergent,
-              memoryFile = s"./test_run_dir/window_$numPoints.txt",
-              windowFunc = window
-            )
-
-            it should "pass when: \n" +
-              s"\t\twindow enabled = $enable,\n" +
-              s"\t\twindow type    = ${window.toString},\n" +
-              s"\t\twindow size    = $numPoints,\n" +
-              s"\t\tconst window   = $constWindow,\n" +
-              s"\t\trun-time       = $runTime,\n" in {
-              val lazyDut = LazyModule(
-                new WindowingTL[FixedPoint](
-                  csrAddress = csrAddress,
-                  ramAddress = ramAddress,
-                  params = params,
-                  beatBytes = beatBytes
-                ) with TestStandaloneTLBlock {
-                  override def standaloneParams: TLBundleParameters =
-                    TLBundleParameters(
-                      addressBits = beatBytes*8,
-                      dataBits = beatBytes*8,
-                      sourceBits = 4,
-                      sinkBits = 1,
-                      sizeBits = 2,
-                      echoFields = Nil,
-                      requestFields = Nil,
-                      responseFields = Nil,
-                      hasBCE = false
-                    )
-                  override def dataBytes: Int = 4
-                }
-              )
-
-              test(lazyDut.module)
-                .withAnnotations(annotations)
-                .runPeekPoke(_ =>
-                  new WindowingTLTester(
-                    lazyDut,
-                    csrAddress = csrAddress,
-                    ramAddress = ramAddress,
-                    params = params,
-                    windowFuncRunTime = BlackmanWindow(numPoints / 2, periodic = true),
-                    beatBytes = beatBytes,
-                    enable = enable,
-                    verbose = verbose,
-                    random = random
-                  )
+            for (trimType <- Seq(Floor, Ceiling, Convergent, Round)) {
+              for (binPoint <- Seq(10, 12, 14)) {
+                // Address
+                val ramAddress = AddressSet(0x60000, numPoints * 4 - 1)
+                val csrAddress = AddressSet(0x60000 + numPoints * 4, 0xF)
+                // Parameters
+                val params = WindowingParams.fixed(
+                  numPoints   = numPoints,
+                  dataWidth   = binPoint + 2,
+                  binPoint    = binPoint,
+                  runTime     = runTime,
+                  constWindow = constWindow,
+                  trimType    = trimType,
+                  memoryFile  = s"./test_run_dir/window_$numPoints.txt",
+                  windowFunc  = window
                 )
+
+                it should "pass when: \n" +
+                  s"\t\twindow enabled = $enable,\n" +
+                  s"\t\twindow type    = ${window.toString},\n" +
+                  s"\t\twindow size    = $numPoints,\n" +
+                  s"\t\tconst window   = $constWindow,\n" +
+                  s"\t\trun-time       = $runTime,\n" +
+                  s"\t\ttrim type      = $trimType,\n" +
+                  s"\t\tbinary point   = $binPoint\n" in {
+                  val lazyDut = LazyModule(
+                    new WindowingTL[FixedPoint](
+                      csrAddress = csrAddress,
+                      ramAddress = ramAddress,
+                      params = params,
+                      beatBytes = beatBytes
+                    ) with TestStandaloneTLBlock {
+                      override def standaloneParams: TLBundleParameters =
+                        TLBundleParameters(
+                          addressBits    = beatBytes * 8,
+                          dataBits       = beatBytes * 8,
+                          sourceBits     = 4,
+                          sinkBits       = 1,
+                          sizeBits       = 2,
+                          echoFields     = Nil,
+                          requestFields  = Nil,
+                          responseFields = Nil,
+                          hasBCE         = false
+                        )
+
+                      override def dataBytes: Int = 4
+                    }
+                  )
+
+                  test(lazyDut.module)
+                    .withAnnotations(annotations)
+                    .runPeekPoke(_ =>
+                      new WindowingTLTester(
+                        lazyDut,
+                        csrAddress = csrAddress,
+                        ramAddress = ramAddress,
+                        params = params,
+                        windowFuncRunTime = BlackmanWindow(numPoints / 2, periodic = true),
+                        beatBytes = beatBytes,
+                        enable = enable,
+                        verbose = verbose,
+                        random = random
+                      )
+                    )
+                }
+              }
             }
           }
         }
