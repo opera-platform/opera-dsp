8789f08a7a4cb10876b7f48beb750ee450982bf7
pznikola <p.z.nikola@etf.rs>
2025-06-24 15:56:51 +0200

    Fixed handshake

--- log-magnitude/src/main/scala/MagnitudeSquared.scala
+++ log-magnitude/src/main/scala/MagnitudeSquared.scala
@@ -2,32 +2,93 @@ package opera.logmagnitude
 
 import chisel3._
 import chisel3.stage.ChiselGeneratorAnnotation
-import chisel3.util.{circt => _, _}
+import chisel3.util.{circt => _}
 import circt.stage.{ChiselStage, FirtoolOption}
 import dsptools._
 import dsptools.numbers._
 import fixedpoint.{FixedPoint, fromIntToBinaryPoint}
 
 class MagnitudeSquared[T <: Data: Real: BinaryRepresentation](val params: LogMagnitudeParams[T]) extends Module {
+  val addPipeRegs: Int = if (params.addPipeRegs) 1 else 0
+  val mulPipeRegs: Int = if (params.mulPipeRegs) 1 else 0
+
   // IO
-  val io = IO(new LogMagnitudeIO(params))
+  val io: LogMagnitudeIO[T] = IO(new LogMagnitudeIO(params))
 
   // Get I (real) and Q (imaginary) absolute values
-  val absI = Real[T].abs(io.in.bits.real)
-  val absQ = Real[T].abs(io.in.bits.imag)
+  val absI: T = Real[T].abs(io.in.bits.real)
+  val absQ: T = Real[T].abs(io.in.bits.imag)
 
-  val A: T = DspContext.alter(
-    DspContext.current
-      .copy(numAddPipes = params.addPipeRegs, numMulPipes = params.mulPipeRegs, binaryPointGrowth = params.binaryGrowth)
-  ) {
-      (absI.context_*(absI)).context_+(absQ.context_*(absQ))
+  // I*I
+  private val squareI = DspContext.alter(DspContext.current.copy(
+    binaryPointGrowth = params.binaryGrowth, trimType = params.trimType
+  )) {
+    absI.context_*(absI)
+  }
+  private val r_squareI = if (params.mulPipeRegs) Some(Reg(squareI.cloneType)) else None
+
+  // Q*Q
+  private val squareQ = DspContext.alter(DspContext.current.copy(
+    binaryPointGrowth = params.binaryGrowth, trimType = params.trimType
+  )) {
+    absQ.context_*(absQ)
+  }
+  private val r_squareQ = if (params.mulPipeRegs) Some(Reg(squareQ.cloneType)) else None
+
+  // I*I + Q*Q
+  private val sumSquares = DspContext.alter(DspContext.current.copy(
+    trimType = params.trimType
+  )) {
+      if (params.mulPipeRegs)
+        r_squareI.get.context_+(r_squareQ.get)
+      else
+        squareI.context_+(squareQ)
+  }
+  private val r_sumSquares = if (params.addPipeRegs) Some(Reg(sumSquares.cloneType)) else None
+
+  private val A =
+    if (params.addPipeRegs)
+      r_sumSquares.get
+    else
+      sumSquares
+
+  // Calculate the number of bits that needs to be trimmed
+  private val trimBits = if ((A.getWidth - params.outputType.getWidth) > 0) A.getWidth - params.outputType.getWidth else 0
+
+  private val trimA = DspContext.alter(DspContext.current.copy(
+    binaryPointGrowth = 0, trimType = params.trimType
+  )) {
+    A.div2(trimBits)
   }
 
-  val w_out = Wire(io.out.cloneType)
-  w_out.bits := A
-  w_out.valid := io.in.valid
-  io.in.ready := w_out.ready
-  AlignHandshake(2 * params.addPipeRegs, w_out, io.out) := A
+  io.out.bits := trimA.asTypeOf(io.out.bits)
+
+  // Handshake control
+  if (params.addPipeRegs || params.mulPipeRegs) {
+    val handshake = AlignHandshake(addPipeRegs + mulPipeRegs, io.in.valid, io.out.ready)
+    for (i <- 0 until addPipeRegs + mulPipeRegs) {
+      when(handshake._1(i)) {
+        if (i == 0) {
+          if (params.mulPipeRegs) {
+            r_squareI.get := squareI
+            r_squareQ.get := squareQ
+          }
+          else {
+            r_sumSquares.get := sumSquares
+          }
+        }
+        else {
+          r_sumSquares.get := sumSquares
+        }
+      }
+    }
+    io.in.ready := handshake._1.head
+    io.out.valid := handshake._2.last
+  }
+  else {
+    io.out.valid := io.in.valid
+    io.in.ready := io.out.ready
+  }
 
 }
 
@@ -37,8 +98,8 @@ object MagnitudeSquaredApp extends App {
     inputType    = DspComplex(FixedPoint(16.W, 14.BP)),
     outputType   = FixedPoint(16.W, 14.BP),
     magType      = Squared,
-    addPipeRegs  = 1,
-    mulPipeRegs  = 1,
+    addPipeRegs  = true,
+    mulPipeRegs  = true,
     binaryGrowth = 0,
     trimType     = Convergent
   )
