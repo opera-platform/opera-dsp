0828d744e7229e57538b00d848b11a114a39c6e2
pznikola <p.z.nikola@etf.rs>
2025-08-06 16:10:19 +0200

    Fixed last signal generation and code cleanup

--- fft/src/main/scala/BitReverse.scala
+++ fft/src/main/scala/BitReverse.scala
@@ -1,20 +1,19 @@
 package opera.fft
 
+import _root_.circt.stage.{ChiselStage, FirtoolOption}
 import breeze.numerics.pow
 import chisel3._
 import chisel3.experimental.requireIsChiselType
 import chisel3.stage.ChiselGeneratorAnnotation
 import chisel3.util._
-import _root_.circt.stage.{ChiselStage, FirtoolOption}
 import dspblocks._
-import dsptools.numbers.{BinaryRepresentation, DspComplex, Real, binaryRepresentationOps}
+import dsptools.numbers.{BinaryRepresentation, DspComplex, Real}
 import fixedpoint.{FixedPoint, fromIntToBinaryPoint}
 
-// TODO: FIX ready/valid!
 case class BitReverseParams[T <: Data](
-  dataType: DspComplex[T], // data type
-  memDepth:   Int, // ping pong memDepth or fft memDepth
-  runTime: Boolean = false,
+  dataType     : DspComplex[T],
+  memDepth     : Int,
+  runTime      : Boolean = false,
   singlePortMem: Boolean = false
 ) {
   requireIsChiselType(dataType)
@@ -48,7 +47,6 @@ class BitReverseIO[T <: Data: Real](val params: BitReverseParams[T]) extends Bun
 class BitReverse[T <: Data: Real: BinaryRepresentation](val params: BitReverseParams[T]) extends Module {
   val io: BitReverseIO[T] = IO(new BitReverseIO(params))
 
-  private val w_valid = Wire(Bool())
   // Double buffer
   val memDepth: Int        = params.memDepth
   private val memories     = Seq.fill(2) { SyncReadMem(memDepth, params.dataType) } // Double buffer
@@ -56,32 +54,35 @@ class BitReverse[T <: Data: Real: BinaryRepresentation](val params: BitReversePa
   private val w_mem_data_2 = Wire(params.dataType)
   
   // FSM
-  private object FSM extends ChiselEnum {
-    val s_idle, s_write, s_write_read, s_read = Value
-  }
+  private object FSM extends ChiselEnum { val s_idle, s_write, s_write_read, s_read = Value }
   private val r_state = RegInit(FSM.s_idle)
 
   // Logic and Counters for reading and writing to memories
+  private val w_valid      = Wire(Bool()) // data is valid for reading
+  private val w_last       = Wire(Bool()) // last flag for output
+  private val w_out        = Wire(UInt((io.in.bits.getWidth + 1).W)) // Helper variable to put data and last into a queue
   private val w_wr_cnt_rst = Wire(Bool()) // write counter reset
   private val w_rd_cnt_rst = Wire(Bool()) // read counter reset
   private val w_wr_wrap    = Wire(Bool()) // wrap the value of write counter
   private val w_rd_wrap    = Wire(Bool()) // wrap the value of read counter
-  private val w_rd_en      = Wire(Bool())
-  private val r_wr_cnt     = CounterWithReset(io.in.fire, memDepth, w_wr_cnt_rst)._1 // write counter with reset
+  private val w_wr_en      = Wire(Bool()) // Write counter enable
+  private val w_rd_en      = Wire(Bool()) // Read counter enable
+  private val r_wr_cnt     = CounterWithReset(w_wr_en, memDepth, w_wr_cnt_rst)._1 // write counter with reset
   private val r_rd_cnt     = CounterWithReset(w_rd_en, memDepth, w_rd_cnt_rst)._1 // read counter with reset
   private val w_rd_addr: UInt = Wire(r_rd_cnt.cloneType)
   private val w_wr_addr: UInt = Wire(r_wr_cnt.cloneType)
   private val r_wr_mem_sel: Bool = RegInit(false.B) // select in which memory to write
   private val r_rd_mem_sel: Bool = RegInit(false.B) // select from which memory to read
-  dontTouch(r_wr_cnt)
-  dontTouch(r_rd_cnt)
-  dontTouch(w_wr_cnt_rst)
-  dontTouch(w_rd_cnt_rst)
   // Conditions to wrap and reset counters
   w_wr_wrap    := r_wr_cnt === (io.i_samples.getOrElse(memDepth.U) - 1.U) && io.in.fire
   w_rd_wrap    := r_rd_cnt === (io.i_samples.getOrElse(memDepth.U) - 1.U) && io.out.fire
   w_wr_cnt_rst := w_wr_wrap || reset.asBool
   w_rd_cnt_rst := w_rd_wrap || reset.asBool
+  w_wr_en      := io.in.fire
+  w_rd_en      := w_valid && io.out.ready
+  w_valid      := r_state === FSM.s_write_read || r_state === FSM.s_read
+  w_last       := RegNext(w_rd_wrap)
+  w_out        := Cat(w_last, Mux(RegNext(r_rd_mem_sel), w_mem_data_2, w_mem_data_1).asUInt)
 
   // Generate write and read address
   private val fftStageNumber = log2Ceil(memDepth)
@@ -110,18 +111,16 @@ class BitReverse[T <: Data: Real: BinaryRepresentation](val params: BitReversePa
     }
   }.elsewhen(r_state === FSM.s_write_read) {
     // We can read from one memory and write to another
-    // Change state
-    when((w_rd_wrap && !w_wr_wrap)) {
+    when(w_rd_wrap && !w_wr_wrap) {
       r_state := FSM.s_write
     }.elsewhen(!w_rd_wrap && w_wr_wrap) {
       r_state := FSM.s_read
     }
   }
-  io.in.ready := r_state =/= FSM.s_read
-  w_valid := r_state === FSM.s_write_read || r_state === FSM.s_read
   when (w_rd_wrap) { r_rd_mem_sel := !r_rd_mem_sel}
   when (w_wr_wrap) { r_wr_mem_sel := !r_wr_mem_sel}
 
+  // Read and/or write to buffers
   if (params.singlePortMem) {
     val w_address_1 = Mux(r_wr_mem_sel, w_wr_addr, w_rd_addr)
     val w_address_2 = Mux(!r_wr_mem_sel, w_wr_addr, w_rd_addr)
@@ -144,14 +143,15 @@ class BitReverse[T <: Data: Real: BinaryRepresentation](val params: BitReversePa
     w_mem_data_2 := memories.last(w_rd_addr)
   }
 
-  val b_queue = Module(new Queue(chiselTypeOf(io.out.bits), entries = 1, pipe = false, flow = true))
-  b_queue.io.enq.bits := Mux(RegNext(r_rd_mem_sel), w_mem_data_2, w_mem_data_1)
-  w_rd_en := w_valid && io.out.ready
-  b_queue.io.enq.valid := RegNext(w_valid && io.out.ready ,false.B)
-  io.out.bits  := b_queue.io.deq.bits
-  io.out.valid := b_queue.io.deq.valid
-  b_queue.io.deq.ready := io.out.ready
-  io.o_last := w_rd_wrap
+  // output logic
+  private val buffer = Module(new Queue(chiselTypeOf(w_out), entries = 1, pipe = false, flow = true))
+  buffer.io.enq.bits  := w_out
+  buffer.io.enq.valid := RegNext(w_valid && io.out.ready ,false.B)
+  io.out.valid        := buffer.io.deq.valid
+  buffer.io.deq.ready := io.out.ready
+  io.out.bits := buffer.io.deq.bits(buffer.io.deq.bits.getWidth - 2, 0).asTypeOf(io.out.bits)
+  io.o_last   := buffer.io.deq.bits(buffer.io.deq.bits.getWidth - 1)
+  io.in.ready := r_state =/= FSM.s_read
 }
 
 object BitReverseApp extends App {
