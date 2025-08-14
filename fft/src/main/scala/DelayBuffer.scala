000626d17ef9eb13432ea40656ac1a6b603b8339
pznikola <p.z.nikola@etf.rs>
2025-08-14 11:51:15 +0200

    Added DelayBuffer

---
+++ fft/src/main/scala/DelayBuffer.scala
@@ -0,0 +1,17 @@
+package opera.fft
+
+import chisel3._
+import chisel3.util.ShiftRegister
+import craft.ShiftRegisterMem
+
+// Shift register used as delay buffers
+object DelayBuffer {
+  def apply[T <: Data](in: T, n: Int, en: Bool = true.B, use_sp_mem: Boolean = false, mem: Boolean = false, name: String = null): T = {
+    if (mem) {
+      ShiftRegisterMem(in, n, en, use_sp_mem, name)
+    }
+    else {
+      ShiftRegister(in, n, en)
+    }
+  }
+}
